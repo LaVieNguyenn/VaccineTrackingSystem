6f58c728128cf1359e6dc6da4e80976526d8d853
Thien817 <thienndse173057@fpt.edu.vn>
2025-03-27 17:33:23 +0700

    Delete VaccineTrackingSystem directory

--- VaccineTrackingSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VaccineTrackingSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
