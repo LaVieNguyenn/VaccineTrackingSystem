d5b169c7e29fa8dad09f107eb9b989852a77a2aa
Thien817 <thienndse173057@fpt.edu.vn>
2025-03-27 18:50:52 +0700

    Add files via upload

---
+++ VaccineTrakingSystem.DAL/Models/Feedback.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VaccineTrakingSystem.DAL.Models;
+
+public partial class Feedback
+{
+    public int FeedbackId { get; set; }
+
+    public int? AppointmentId { get; set; }
+
+    public int CustomerId { get; set; }
+
+    public int Rating { get; set; }
+
+    public string? Comments { get; set; }
+
+    public DateTime FeedbackDate { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual Appointment? Appointment { get; set; }
+
+    public virtual User Customer { get; set; } = null!;
+}
