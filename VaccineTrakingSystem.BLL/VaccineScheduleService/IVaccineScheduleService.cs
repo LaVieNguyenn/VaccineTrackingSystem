5f000d23f13583f818fc966f621d556ab07e23a8
Thien817 <thienndse173057@fpt.edu.vn>
2025-04-01 23:27:29 +0700

    Add files via upload

---
+++ VaccineTrakingSystem.BLL/VaccineScheduleService/IVaccineScheduleService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccineTrakingSystem.DAL.Models;
+
+namespace VaccineTrakingSystem.BLL.VaccineScheduleService
+{
+    public interface IVaccineScheduleService
+    {
+        Task<IEnumerable<VaccineSchedule>> GetAllVaccineScheduleServiceAsync();
+        Task<VaccineSchedule?> GetVaccineScheduleServiceByIdAsync(int id);
+        Task<int> CreateVaccineScheduleServiceAsync(VaccineSchedule vaccineSchedule);
+        Task<bool> UpdateVaccineScheduleServiceAsync(VaccineSchedule vaccineSchedule);
+        Task<bool> DeleteVaccineScheduleServiceAsync(int id);
+    }
+}
---
+++ VaccineTrakingSystem.BLL/VaccineScheduleService/VaccineScheduleService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccineTrakingSystem.DAL.Models;
+using VaccineTrakingSystem.DAL.Repositories;
+
+namespace VaccineTrakingSystem.BLL.VaccineScheduleService
+{
+    public class VaccineScheduleService : IVaccineScheduleService
+    {
+
+        private readonly IGenericRepository<VaccineSchedule> _repository;
+
+        public VaccineScheduleService(IGenericRepository<VaccineSchedule> repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<int> CreateVaccineScheduleServiceAsync(VaccineSchedule vaccineSchedule)
+        {
+            return _repository.InsertAsync(vaccineSchedule);
+        }
+
+        public Task<bool> DeleteVaccineScheduleServiceAsync(int id)
+        {
+            return _repository.DeleteAsync(id);
+        }
+
+        public Task<IEnumerable<VaccineSchedule>> GetAllVaccineScheduleServiceAsync()
+        {
+            return _repository.GetAllAsync();
+        }
+
+        public Task<VaccineSchedule?> GetVaccineScheduleServiceByIdAsync(int id)
+        {
+            return _repository.GetByIdAsync(id);
+        }
+
+        public Task<bool> UpdateVaccineScheduleServiceAsync(VaccineSchedule vaccineSchedule)
+        {
+            return _repository.UpdateAsync(vaccineSchedule);
+        }
+    }
+}
